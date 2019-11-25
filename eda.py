264d87a0070b3959542740453f427becef73567d
meamarp <appotdar05@gmail.com>
2019-11-25 11:48:14 +0530

    added func to display img data

--- eda.py
+++ eda.py
@@ -31,6 +31,31 @@ def rle_to_mask(rle_string, height, width):
         img = img.T
         return img
 
+def displayImageData(in_df,no_of_sample=9):
+    """Disaplay Sample Image data
+    
+    Arguments:
+        in_df {pandas dataframe} -- Input train Dataframe with Img_Path Column
+    
+    Keyword Arguments:
+        no_of_sample {int} -- Number of Image samples to display (default: {9})
+    """
+    fig = plt.figure("EDA Image Data")
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+    fig.suptitle('Understanding Image Data', fontsize=16)
+
+    
+    for n, path in enumerate(in_df.img_id[:no_of_sample]):
+        label = str(in_df.class_id[n])
+        ImgTitle = in_df.img_id[n] +'-'+label
+        Img = cv2.imread('severstal-steel-defect-detection/train_images/'+path)
+        #Note: Adjust the subplot grid so nb_of_images fits pefect ina grid.
+        ax = fig.add_subplot(3,3,(n+1))
+        plt.imshow(cv2.cvtColor(Img, cv2.COLOR_BGR2RGB))
+        ax.set_title(ImgTitle)
+        plt.axis('off')
+    plt.show()
+    return
 
 
 # path_to_img = r'severstal-steel-defect-detection\train_images\0a1cade03.jpg'
@@ -41,4 +66,8 @@ train_df['img_id'] = train_df.ImageId_ClassId.str.split("_", n = 1, expand = Tru
 train_df['class_id'] = train_df.ImageId_ClassId.str.split("_", n = 1, expand = True)[1]
 train_df= train_df.drop(['ImageId_ClassId'],axis=1)
 train_df = train_df.dropna()
+train_df = train_df.reset_index(drop=True)
+print(train_df.head())
 
+# r'severstal-steel-defect-detection\train_images\0a1cade03.jpg'
+displayImageData(train_df)
